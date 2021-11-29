325161a5dd366254cd0c8c6b4250ab7b098dbf52
KERRCAM <kerrcameron@hotmail.com>
2021-11-29 14:03:29 +0000

    inputs

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
+
+
 using namespace std;
 
 int main() {
 
     int age;
-    cout << "enter your age" << endl;
+    cout << "enter your age: ";
     cin >> age;
     cout << "you are " << age << " years old" << endl;
+    // cin can be used for other data types as well
+    string name;
+    cout << "enter your name: ";
+    cin >> name;
+    cout << "hello " << name << endl;
 
 
     return 0;
 }
+
