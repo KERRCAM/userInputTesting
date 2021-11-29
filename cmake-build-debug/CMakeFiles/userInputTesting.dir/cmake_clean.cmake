fde3172b99a0979e658c38071afaa32db36ecd9a
KERRCAM <kerrcameron@hotmail.com>
2021-11-29 13:50:14 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/userInputTesting.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/userInputTesting.dir/main.cpp.obj"
+  "userInputTesting.exe"
+  "userInputTesting.exe.manifest"
+  "userInputTesting.lib"
+  "userInputTesting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/userInputTesting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
